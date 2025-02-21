93fd35bff807ff77510ed351d4eaa5c0350e9f80
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-02-21 14:48:50 +0530

    vbjbjn 21.02

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,42 +1,42 @@
-// Import necessary dependencies
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './components/layout-components/theme-provider'
-import { Toaster } from './components/ui/sonner'
 import routes from './routes/routes'
 import { GlobalStateProvider } from './context/global-state-context'
+import { Toaster } from 'react-hot-toast'
 
 // Create a new instance of QueryClient with default options for queries
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 0 // Set the default staleTime to 0 for queries (no caching)
-    }
-  }
-})
 
 // Main App component
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      {/* ThemeProvider is used to manage theme across the app (default "dark" theme) */}
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        {/* GlobalStateProvider manages global state across the application */}
-        <GlobalStateProvider>
-          {/* RouterProvider is used to manage routing for the application */}
-          <RouterProvider router={routes} />
-        </GlobalStateProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {/* GlobalStateProvider manages global state across the application */}
+      <GlobalStateProvider>
+        {/* RouterProvider is used to manage routing for the application */}
+        <RouterProvider router={routes} />
+      </GlobalStateProvider>
 
-        {/* Toaster is used to show toast notifications */}
-        <Toaster richColors position="top-center" />
-      </ThemeProvider>
-
-      {/* ReactQueryDevtools provides debugging tools for React Query */}
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+      {/* Toaster is used to show toast notifications */}
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 3000
+          },
+          error: {
+            duration: 5000
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px'
+          }
+        }}
+      />
+    </ThemeProvider>
   )
 }
 
-// Export the App component
 export default App
